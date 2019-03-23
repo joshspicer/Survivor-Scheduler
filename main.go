f48d29af6f3b0d975cbea98236fb68ec235e0395
Josh Spicer <hello@joshspicer.com>
2019-03-23 17:04:09 -0400

    WOW! Fields of structs are only public in Go if they start with a capital.

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -18,18 +21,18 @@ import (
 Defines a .survive file datatype.
 */
 type SurviveFile struct {
-	category  int // Will divide between player data, global data, etc..
+	Category  int // Will divide between player data, global data, etc..
 	week      int
-	title     string
-	Body      string
-	createdAt time.Time
+	Title     string
+	Body      string /////TODO: use the "bits per half hour" idea sketched out. convert to hex for coolness?
+	CreatedAt time.Time
 }
 
 // ENVIRONMENT VARIABLES
 const ENV_ROOT = "/Users/joshspicer/go/src/github.com/joshspicer/survivor-scheduler"
 
 func (ss *SurviveFile) save() error {
-	filename := fmt.Sprintf("%s/c%d-w%d-%s.survive", ENV_ROOT, ss.category, ss.week, ss.title)
+	filename := fmt.Sprintf("%s/c%d-w%d-%s.survive", ENV_ROOT, ss.Category, ss.week, ss.Title)
 
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 
@@ -40,7 +43,7 @@ func (ss *SurviveFile) save() error {
 		return err
 	}
 
-	stringToWrite := fmt.Sprintf("%s: %s%s", ss.createdAt.Format(time.RFC3339), cleanedBody, "\n")
+	stringToWrite := fmt.Sprintf("%s: %s%s", ss.CreatedAt.Format(time.RFC3339), cleanedBody, "\n")
 	_, err = f.WriteString(stringToWrite)
 	if err != nil {
 		log.Fatal("Error writing to file.", err)
@@ -50,34 +53,43 @@ func (ss *SurviveFile) save() error {
 	return nil
 }
 
+func loadPage(category int, week int, title string) SurviveFile {
+	filename := fmt.Sprintf("%s/c%d-w%d-%s.survive", ENV_ROOT, category, week, title)
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Error loading page!")
+		return SurviveFile{}
+	}
+	return SurviveFile{Category: category, week: week, Title: title, Body: string(body)}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p := loadPage(4, 55, title)
+	fmt.Print(p)
+
+	//if err != nil {
+	//	log.Fatal("Could not load page view handler")
+	//	//TODO: redirect back home?
+	//}
+	t, _ := template.ParseFiles("templates/home.html")
+	err := t.Execute(w, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 /*
 Main function. Entry point of program.
 */
 func main() {
 
-	//templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
-	//http.Handle("/static/", //final url can be anything
-	//	http.StripPrefix("/static/",
-	//		http.FileServer(http.Dir("static"))))
-
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//
-	//	//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
-	//	if name := r.FormValue("name"); name != "" {
-	//		welcome.Name = name
-	//	}
-	//	//If errors show an internal server error message
-	//	//I also pass the welcome struct to the welcome-template.html file.
-	//	if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
-	//		http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	}
-	//})
-
-	t1 := SurviveFile{category: 4, week: 55, title: "hello", Body: "This is :a sampl: :::e Page.::", createdAt: time.Now()}
+	t1 := SurviveFile{Category: 4, week: 55, Title: "hello", Body: "This is a sample page.", CreatedAt: time.Now()}
 	err := t1.save()
 	fmt.Print(err)
 
-	//fmt.Println("Listening")
-	//fmt.Println(http.ListenAndServe(":25500", nil))
-
+	http.HandleFunc("/view/", viewHandler)
+	//http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
